51c0c4c3de62d7bdbe5a54f3d395ea1101670c3b
BilelHimeur <bilelhimeur@gmail.com>
2018-12-28 15:53:27 +0100

    Tuple examples

---
+++ src/main/java/examples/Tuple_examples.java
@@ -0,0 +1,41 @@
+package examples;
+
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.Tuple3;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tuple_examples {
+    // Tuples are immutable and can hold multiple objects of different types in a type-safe manner.
+    // Tuples are of type Tuple1, Tuple2 to Tuple8 depending on the number of elements they are to take.
+
+    @Test
+    public void whenCreatesTuple_thenCorrect1() {
+        Tuple2<String, Integer> java8 = Tuple.of("Java", 8);
+        String element1 = java8._1;
+        int element2 = java8._2();
+
+        assertEquals("Java", element1);
+        assertEquals(8, element2);
+    }
+
+    @Test
+    public void whenCreatesTuple_thenCorrect2() {
+        Tuple3<String, Integer, Double> java8 = Tuple.of("Java", 8, 1.8);
+        String element1 = java8._1;
+        int element2 = java8._2();
+        double element3 = java8._3();
+
+        assertEquals("Java", element1);
+        assertEquals(8, element2);
+        assertEquals(1.8, element3, 0.1);
+    }
+
+    /*
+     A tuple’s place is in storing a fixed group of objects of any type that are better processed as a unit
+     and can be passed around. A more obvious use case is returning more than one object from a function
+     or a method in Java.
+    * */
+}
