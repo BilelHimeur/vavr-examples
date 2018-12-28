869926fc0d1e81eefd7bb49c32cebfe54a5100f6
BilelHimeur <bilelhimeur@gmail.com>
2018-12-28 14:55:04 +0100

    upload projet (first commit)

---
+++ src/main/java/com/vavr/vavrExamples/VavrExamplesApplication.java
@@ -0,0 +1,14 @@
+package com.vavr.vavrExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VavrExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VavrExamplesApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/vavr/vavrExamples/VavrExamplesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vavr.vavrExamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VavrExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
